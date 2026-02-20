619641c4afef9ddd9f54ed1fc625a0d4dfeb8f34
srinivas reddy <srinivas@Services-MacBook-Pro-2.local>
2026-02-20 17:29:22 +0530

    Refactor CompanyProvisioningService - improve schema provisioning flow

--- FinpayAPI/app/services/company_provisioning_service.rb
+++ FinpayAPI/app/services/company_provisioning_service.rb
@@ -1,22 +1,28 @@
 class CompanyProvisioningService
 
-  # This service is responsible for provisioning a new schema for a company when it is created.
-  # schema defines strucuture of the database and it is used to isolate data for different tenants in a multi-tenant application.
-  # When a new company is created, this service will create a new schema for that company and load the necessary database structure into it.
   def self.call(company)
-    # Generate a unique schema name based on the company ID
-    schema_name = "company_#{company.id}"
+    schema_name = company.subdomain
 
     # Create a new schema for the company
-    ActiveRecord::Base.connection.execute(
-      "CREATE SCHEMA #{schema_name}"
-    )
+    ActiveRecord::Base.connection.execute("CREATE SCHEMA #{schema_name}")
 
     # Switch to the new schema for the current connection
     ActiveRecord::Base.connection.schema_search_path = schema_name
 
-    # Load the schema for the new tenant
+    # Load the schema for the new tenant (loads application's database schema into the new tenant's schema)
     load Rails.root.join("db/schema.rb")
-  end
 
+  ensure
+    # Always reset the schema search path to "public" after provisioning to avoid cross-request contamination.
+    ActiveRecord::Base.connection.schema_search_path = "public"
+  end
 end
+
+=begin
+automatically create schema for new company using Apartment gem:
+class CompanyProvisioningService
+  def self.call(company)
+    Apartment::Tenant.create(company.subdomain)
+  end
+end
+=end
